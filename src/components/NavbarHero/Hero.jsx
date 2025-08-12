0d39dc10ba3daac33df82ed192f3dcf91802a29b
Inna P <nonamer099@gmail.com>
2025-08-12 17:13:48 +0300

    hero avatar compressed

--- src/components/NavbarHero/Hero.jsx
+++ src/components/NavbarHero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../layout.scss";
 import "./Hero.scss";
-import photo from "../../assets/Photo.svg";
+import photo from "../../assets/Photo1.webp";
 import { useTranslation } from "react-i18next";
 import "../../components/help_classes.scss";
 import SimpleTypeWriter from "../Interface/SimpleTypeWriter";
